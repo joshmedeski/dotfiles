9d711d300e3e7508af157d0950e6553e7f25014d
Josh Medeski <josh.medeski@gmail.com>
2021-03-07 14:07:17 -0600

    feat: adjust brew install script

--- bin/brew-install.sh
+++ bin/brew-install.sh
@@ -21,8 +21,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # neovim
 # https://github.com/neovim/neovim/wiki/Installing-Neovim
-brew install --HEAD luajit
-brew install neovim --HEAD
+brew install neovim
 # https://github.com/junegunn/vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -48,6 +47,7 @@ brew install starship
 brew install zoxide
 brew install pgcli
 brew install tldr
+brew install wakatime-cli
 
 if [[ $PLATFORM == 'macos' ]]; then
    brew install trash-cli
