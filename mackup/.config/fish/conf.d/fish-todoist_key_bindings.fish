647f4286d9669c5441b5b1595015a22d977eb53c
Josh Medeski <josh.medeski@gmail.com>
2021-07-02 09:40:00 -0500

    feat[fish]: remove unused todoist fish plugins

--- mackup/.config/fish/conf.d/fish-todoist_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-bind \ct '__todoist_actions'
-if bind -M insert >/dev/null ^/dev/null
-    bind -M insert \ct '__todoist_actions'
-end
-
--- mackup/.config/fish/functions/fzf_todoist_project.fish
+++
@@ -1,7 +0,0 @@
-function fzf_todoist_project
-  todoist projects | fzf | head -n1 | cut -d ' ' -f 1 | read ret
-  if [ $ret ]
-    set buf (commandline | sed -e 's/[ \t]*$//')
-    commandline "$buf -P $ret"
-  end
-end
