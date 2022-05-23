617b2dd9ad243eacf0ce63d31b4635af4b591260
Josh Medeski <josh.medeski@gmail.com>
2022-05-23 17:34:08 -0500

    feat[fish]: use nvimpager as default PAGER

--- mackup/.config/fish/config.fish
+++ mackup/.config/fish/config.fish
@@ -4,6 +4,7 @@ zoxide init fish | source
 
 set -Ux BAT_THEME Nord 
 set -Ux EDITOR nvim
+set -Ux PAGER ~/.local/bin/nvimpager
 set -Ux FZF_CTRL_R_OPTS "--reverse --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 set -Ux FZF_DEFAULT_OPTS "--color=spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
