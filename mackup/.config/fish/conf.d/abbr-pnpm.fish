2ef628012560942fbc6cf215c9ef76fa3ebd4d60
Josh Medeski <josh.medeski@gmail.com>
2022-05-30 21:49:21 -0500

    feat: add pnpm abbreviations

---
+++ mackup/.config/fish/conf.d/abbr-pnpm.fish
@@ -0,0 +1,26 @@
+# abbreviations for pnpm
+
+# built-in
+abbr p "pnpm"
+abbr pa "pnpm add"
+abbr pe "pnpm exec"
+abbr ph "pnpm help"
+abbr pi "pnpm install"
+abbr pim "pnpm import"
+abbr pir "pnpm rebuild"
+abbr pit "pnpm install-test"
+abbr pl "pnpm link"
+abbr pls "pnpm ls"
+abbr pr "pnpm run"
+abbr ps "pnpm start"
+abbr psa "pnpm store add"
+abbr psp "pnpm store prune"
+abbr pss "pnpm store status"
+abbr pt "pnpm test"
+abbr pul "pnpm unlink"
+
+# scripts
+abbr pb "pnpm build"
+abbr pd "pnpm dev"
+abbr pe "pnpm e2e"
+abbr pu "pnpm unit"
