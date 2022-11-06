57547fafdb3262feeb95408c0df37aeb49ed82df
Josh Medeski <josh.medeski@gmail.com>
2022-11-05 19:47:34 -0500

    feat: update lvim config

--- mackup/.config/lvim/config.lua
+++ mackup/.config/lvim/config.lua
@@ -185,31 +185,138 @@ lvim.plugins = {
         "ThePrimeagen/harpoon",
         config = function()
             require("harpoon").setup({global_settings = {mark_branch = true}})
-            -- nnoremap <silent><leader>0 :lua require("harpoon.ui").toggle_quick_menu()<CR>
-            -- nnoremap <silent><leader>' :lua require("harpoon.mark").add_file()<CR>
-            -- nnoremap <silent><leader>1 :lua require("harpoon.ui").nav_file(1)<CR>
-            -- nnoremap <silent><leader>2 :lua require("harpoon.ui").nav_file(2)<CR>
-            -- nnoremap <silent><leader>3 :lua require("harpoon.ui").nav_file(3)<CR>
-            -- nnoremap <silent><leader>4 :lua require("harpoon.ui").nav_file(4)<CR>
-            -- nnoremap <silent><leader>5 :lua require("harpoon.ui").nav_file(5)<CR>
-            -- nnoremap <silent><leader>6 :lua require("harpoon.ui").nav_file(6)<CR>
-            -- nnoremap <silent><leader>7 :lua require("harpoon.ui").nav_file(7)<CR>
-            -- nnoremap <silent><leader>8 :lua require("harpoon.ui").nav_file(8)<CR>
-            -- nnoremap <silent><leader>9 :lua require("harpoon.ui").nav_file(9)<CR>
+        end
+    }, {
+        "folke/todo-comments.nvim",
+        config = function()
+            require("todo-comments").setup({
+                signs = true, -- show icons in the signs column
+                sign_priority = 8, -- sign priority
+                -- keywords recognized as todo comments
+                keywords = {
+                    FIX = {
+                        icon = " ", -- icon used for the sign, and in search results
+                        color = "error", -- can be a hex color, or a named color (see below)
+                        alt = {"FIXME", "BUG", "FIXIT", "ISSUE"} -- a set of other keywords that all map to this FIX keywords
+                        -- signs = false, -- configure signs for some keywords individually
+                    },
+                    TODO = {icon = " ", color = "info"},
+                    HACK = {icon = " ", color = "warning"},
+                    WARN = {
+                        icon = " ",
+                        color = "warning",
+                        alt = {"WARNING", "XXX"}
+                    },
+                    PERF = {
+                        icon = " ",
+                        alt = {"OPTIM", "PERFORMANCE", "OPTIMIZE"}
+                    },
+                    NOTE = {icon = " ", color = "hint", alt = {"INFO"}}
+                },
+                merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+                -- highlighting of the line containing the todo comment
+                -- * before: highlights before the keyword (typically comment characters)
+                -- * keyword: highlights of the keyword
+                -- * after: highlights after the keyword (todo text)
+                highlight = {
+                    before = "", -- "fg" or "bg" or empty
+                    keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+                    after = "fg", -- "fg" or "bg" or empty
+                    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+                    comments_only = true, -- uses treesitter to match keywords in comments only
+                    max_line_len = 400, -- ignore lines longer than this
+                    exclude = {} -- list of file types to exclude highlighting
+                },
+                -- list of named colors where we try to extract the guifg from the
+                -- list of hilight groups or use the hex color if hl not found as a fallback
+                colors = {
+                    error = {"DiagnosticError", "ErrorMsg", "#DC2626"},
+                    warning = {"DiagnosticWarning", "WarningMsg", "#FBBF24"},
+                    info = {"DiagnosticInfo", "#2563EB"},
+                    hint = {"DiagnosticHint", "#10B981"},
+                    default = {"Identifier", "#7C3AED"}
+                },
+                search = {
+                    command = "rg",
+                    args = {
+                        "--color=never", "--no-heading", "--with-filename",
+                        "--line-number", "--column"
+                    },
+                    -- regex that will be used to match keywords.
+                    -- don't replace the (KEYWORDS) placeholder
+                    pattern = [[\b(KEYWORDS):]] -- ripgrep regex
+                    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+                }
+            })
+        end
+    }, {
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+        config = function()
+            require("trouble").setup {
+                position = "top", -- position of the list can be: bottom, top, left, right
+                height = 5, -- height of the trouble list when position is top or bottom
+                width = 50, -- width of the list when position is left or right
+                icons = true, -- use devicons for filenames
+                mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+                fold_open = "", -- icon used for open folds
+                fold_closed = "", -- icon used for closed folds
+                group = true, -- group results by file
+                padding = true, -- add an extra new line on top of the list
+                action_keys = { -- key mappings for actions in the trouble list
+                    -- map to {} to remove a mapping, for example:
+                    -- close = {},
+                    close = "q", -- close the list
+                    cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+                    refresh = "r", -- manually refresh
+                    jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+                    open_split = {"<c-x>"}, -- open buffer in new split
+                    open_vsplit = {"<c-v>"}, -- open buffer in new vsplit
+                    open_tab = {"<c-t>"}, -- open buffer in new tab
+                    jump_close = {"o"}, -- jump to the diagnostic and close the list
+                    toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+                    toggle_preview = "P", -- toggle auto_preview
+                    hover = "K", -- opens a small popup with the full multiline message
+                    preview = "p", -- preview the diagnostic location
+                    close_folds = {"zM", "zm"}, -- close all folds
+                    open_folds = {"zR", "zr"}, -- open all folds
+                    toggle_fold = {"zA", "za"}, -- toggle fold of current file
+                    previous = "k", -- preview item
+                    next = "j" -- next item
+                },
+                indent_lines = true, -- add an indent guide below the fold icons
+                auto_open = false, -- automatically open the list when you have diagnostics
+                auto_close = false, -- automatically close the list when you have no diagnostics
+                auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+                auto_fold = false, -- automatically fold a file trouble list at creation
+                auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+                signs = {
+                    -- icons / text used for a diagnostic
+                    error = "",
+                    warning = "",
+                    hint = "",
+                    information = "",
+                    other = "﫠"
+                },
+                use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
+            }
         end
     }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = {"*tmux.conf"},
+    command = "!tmux source <afile>"
+})
+vim.api.nvim_create_autocmd("BufWritePost",
+                            {pattern = {"config.lua"}, command = "LvimReload"})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = {".yabairc"},
+    command = "!brew services restart yabai"
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = {".skhdrc"},
+    command = "!brew services restart skhd"
+})
