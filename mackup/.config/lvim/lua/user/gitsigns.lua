484bfe380e071f5ea0c92e0806727098596d9caa
Josh Medeski <josh.medeski@gmail.com>
2022-12-16 15:48:08 -0600

    feat: don't highlight git number

--- mackup/.config/lvim/lua/user/gitsigns.lua
+++ mackup/.config/lvim/lua/user/gitsigns.lua
@@ -32,7 +32,7 @@ lvim.builtin.gitsigns.opts = {
     }
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = { interval = 1000, follow_files = true },
