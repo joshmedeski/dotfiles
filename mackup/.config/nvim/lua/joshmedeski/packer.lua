0f63df645c1d9ffb375e530d330a0ecb2bc1f9e4
Josh Medeski <josh.medeski@gmail.com>
2022-12-20 23:10:42 -0600

    feat[nvim]: organize plugins more

--- mackup/.config/nvim/lua/joshmedeski/packer.lua
+++ mackup/.config/nvim/lua/joshmedeski/packer.lua
@@ -1,28 +1,12 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup({ function(use)
+    -- plugins
     use 'wbthomason/packer.nvim'
-
-    -- git
-    use 'lewis6991/gitsigns.nvim'
-    use 'rhysd/committia.vim'
-    use {
-        'sindrets/diffview.nvim',
-        requires = { 'nvim-lua/plenary.nvim', 'kyazdani42/nvim-web-devicons' }
-    }
-    use { "pwntester/octo.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
-            "kyazdani42/nvim-web-devicons",
-        },
-        config = function()
-            require "octo".setup {}
-        end
-    }
 
     -- manipulation
     use 'tpope/vim-surround'
+    use 'folke/which-key.nvim'
     use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
 
     -- navigation
@@ -39,10 +23,6 @@ return require('packer').startup({ function(use)
         run = 'make', cond = vim.fn.executable 'make' == 1
     }
 
-    -- help
-    use 'folke/which-key.nvim'
-    use { 'folke/todo-comments.nvim', requires = 'nvim-lua/plenary.nvim' }
-
     -- style
     use 'catppuccin/nvim'
     use 'xiyaowong/nvim-transparent'
@@ -56,14 +36,27 @@ return require('packer').startup({ function(use)
         config = function() require 'colorizer'.setup {} end,
     }
 
-    -- tmux
-    use 'christoomey/vim-tmux-navigator'
-
-    -- tracking
-    use 'wakatime/vim-wakatime'
-    use 'ActivityWatch/aw-watcher-vim'
+    -- git
+    use 'lewis6991/gitsigns.nvim'
+    use 'rhysd/committia.vim'
+    use {
+        'sindrets/diffview.nvim',
+        requires = { 'nvim-lua/plenary.nvim', 'kyazdani42/nvim-web-devicons' }
+    }
+    use {
+        'pwntester/octo.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'kyazdani42/nvim-web-devicons',
+        },
+        config = function()
+            require 'octo'.setup {}
+        end
+    }
 
     -- lsp
+    use { 'folke/todo-comments.nvim', requires = 'nvim-lua/plenary.nvim' }
     use {
         'VonHeikemen/lsp-zero.nvim',
         requires = {
@@ -85,5 +78,12 @@ return require('packer').startup({ function(use)
             { 'folke/neodev.nvim' }
         }
     }
+
+    -- tmux
+    use 'christoomey/vim-tmux-navigator'
+
+    -- tracking
+    use 'wakatime/vim-wakatime'
+    use 'ActivityWatch/aw-watcher-vim'
 end, config = { display = { open_fn = require('packer.util').float } }
 })
