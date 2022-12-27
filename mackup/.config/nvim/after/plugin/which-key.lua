888ec91c09d656ee8a5805dc45aa7380834219b3
Josh Medeski <josh.medeski@gmail.com>
2022-12-27 09:43:15 -0600

    feat[nvim]: remove some which-key defaults and add code action

--- mackup/.config/nvim/after/plugin/which-key.lua
+++ mackup/.config/nvim/after/plugin/which-key.lua
@@ -11,9 +11,9 @@ wk.setup({
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
     -- No actual key bindings are created
     presets = {
-      operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = true, -- adds help for motions
-      text_objects = true, -- help for text objects triggered after entering an operator
+      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = false, -- adds help for motions
+      text_objects = false, -- help for text objects triggered after entering an operator
       windows = true, -- default bindings on <c-w>
       nav = true, -- misc bindings to work with windows
       z = true, -- bindings for folds, spelling and others prefixed with z
@@ -77,6 +77,9 @@ vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank to clipboard" })
 wk.register({
   -- format
   ["="] = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format file" },
+
+  -- lsp
+  ["."] = { "<cmd>CodeActionMenu<cr>", "CodeActionMenu" },
 
   -- Harpoon
   ["'"] = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add to Harpoon" },
