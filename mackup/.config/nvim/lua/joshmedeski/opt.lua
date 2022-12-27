bc5e2d126c6e3c1a20eef9474483fdfb7e48b7dd
Josh Medeski <josh.medeski@gmail.com>
2022-12-27 00:41:07 -0600

    feat[nvim]: adjust options

--- mackup/.config/nvim/lua/joshmedeski/opt.lua
+++ mackup/.config/nvim/lua/joshmedeski/opt.lua
@@ -1,3 +1,6 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- colors
 vim.opt.termguicolors = true
 
@@ -5,12 +8,17 @@ vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- default position
+vim.opt.scrolloff = 5
+
 -- search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 
 -- gutter
 vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- indent
 vim.opt.tabstop = 2
@@ -25,10 +33,13 @@ vim.opt.swapfile = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- spelling
+vim.opt.spell = false
+vim.opt.spelllang = { "en_us" }
+
 -- misc
 vim.opt.guicursor = ""
 vim.opt.isfname:append("@-@")
-vim.opt.scrolloff = 5
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.opt.wrap = false
