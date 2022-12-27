d17df0884052b1c464d3e89cdc675ec4844db3e4
Josh Medeski <josh.medeski@gmail.com>
2022-12-27 09:42:58 -0600

    feat[nvim]: adjust trouble layout

--- mackup/.config/nvim/after/plugin/trouble.lua
+++ mackup/.config/nvim/after/plugin/trouble.lua
@@ -1,9 +1,8 @@
--- TODO: add which-key entries for trouble
 -- TODO: show all TODOs from git root
 
 require("trouble").setup({
-  position = "bottom", -- position of the list can be: bottom, top, left, right
-  height = 10, -- height of the trouble list when position is top or bottom
+  position = "top", -- position of the list can be: bottom, top, left, right
+  height = 5, -- height of the trouble list when position is top or bottom
   width = 50, -- width of the list when position is left or right
   icons = true, -- use devicons for filenames
   mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
