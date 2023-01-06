6a12c70e71b4fee8ee666c8df80755cde73a7ee6
Josh Medeski <josh.medeski@gmail.com>
2023-01-06 12:10:37 -0600

    refactor: format

--- mackup/.config/nvim/lua/joshmedeski/keymap.lua
+++ mackup/.config/nvim/lua/joshmedeski/keymap.lua
@@ -1,2 +1,10 @@
-vim.keymap.set("n", "[g", "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>zt")
-vim.keymap.set("n", "]g", "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>zt")
+vim.keymap.set(
+  "n",
+  "[g",
+  "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>zt"
+)
+vim.keymap.set(
+  "n",
+  "]g",
+  "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>zt"
+)
