eab1f2c8549dbd51928a652c47bfd1a5c51dfb93
Josh Medeski <josh.medeski@gmail.com>
2023-01-23 22:15:35 -0600

    feat: make gitmux conf sh

--- mackup/.config/nvim/lua/joshmedeski/autocmd.lua
+++ mackup/.config/nvim/lua/joshmedeski/autocmd.lua
@@ -21,3 +21,10 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
     vim.cmd([[set filetype=markdown wrap linebreak nolist]])
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  pattern = { "gitmux.conf" },
+  callback = function()
+    vim.cmd([[set filetype=sh]])
+  end,
+})
