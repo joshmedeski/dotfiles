84533318a2944f893ddfc38d18d3f1e6090f7f92
Josh Medeski <josh.medeski@gmail.com>
2023-02-06 20:24:51 -0600

    feat: overwrite null-ls

---
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,34 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      debounce = 150,
+      save_after_format = false,
+      sources = {
+        null_ls.builtins.code_actions.cspell,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.code_actions.refactoring,
+        null_ls.builtins.diagnostics.cspell,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.fish,
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.fish_indent,
+        null_ls.builtins.formatting.mdformat,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.gitsigns,
+
+        null_ls.builtins.diagnostics.selene.with({
+          condition = function(utils)
+            return utils.root_has_file({ "selene.toml" })
+          end,
+        }),
+      },
+      root_dir = require("null-ls.utils").root_pattern("package.json", ".null-ls-root", ".neoconf.json", ".git"),
+    })
+  end,
+}
