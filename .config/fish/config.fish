bf653eeba30492a9f39d172edf0c2ff576151ff8
Josh Medeski <josh.medeski@gmail.com>
2023-03-02 00:11:44 -0600

    feat: add `x` back to variables for executables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@
 # ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
 # A smart and user-friendly command line
 # https://fishshell.com
+# cSpell:words ajeetdsouza cppflags ldflags pkgconfig pnpm nvim Nord gopath nvimpager ripgreprc ripgrep zoxide joshmedeski sharkdp neovim lucc
 
 starship init fish | source # https://starship.rs/
 zoxide init fish | source # 'ajeetdsouza/zoxide'
@@ -16,24 +17,32 @@ set -gx CPPFLAGS -I/opt/homebrew/opt/sqlite/include
 set -gx LDFLAGS -L/opt/homebrew/opt/sqlite/lib
 set -gx PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
 set -gx PNPM_HOME /Users/joshmedeski/Library/pnpm # https://pnpm.io/
-set -U BAT_THEME Nord # 'sharkdp/bat' cat clone 
-set -U EDITOR nvim # 'neovim/neovim' text editor 
-set -U FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
-set -U FZF_DEFAULT_COMMAND "fd -H -E '.git'"
-set -U FZF_DEFAULT_OPTS "--reverse --no-info --prompt=' ' --pointer='' --marker=' ' --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
-set -U FZF_TMUX_OPTS "-p --no-info --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
+set -Ux BAT_THEME Nord # 'sharkdp/bat' cat clone 
+set -Ux EDITOR nvim # 'neovim/neovim' text editor 
+set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
+
+set -Ux FZF_DEFAULT_OPTS "--reverse --no-info --pointer='' --marker='' \
+--ansi --color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
+
+set -Ux FZF_TMUX_OPTS "-p --reverse --no-info --pointer='' --marker='' \
+--ansi --color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
+
+set -Ux FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
+
 set -U GOPATH (go env GOPATH) # https://golang.google.cn/
 set -U KIT_EDITOR /opt/homebrew/bin/nvim # https://www.scriptkit.com/
 set -U LANG en_US.UTF-8
 set -U LC_ALL en_US.UTF-8
 set -U NODE_PATH "~/.nvm/versions/node/v16.19.0/bin/node" # 'nvm-sh/nvm'
 set -U PAGER ~/.local/bin/nvimpager # 'lucc/nvimpager'
+set -U RIPGREP_CONFIG_PATH "$HOME/.config/rg/ripgreprc"
 set -U VISUAL nvim
 
 # ordered by priority - bottom up
 fish_add_path /opt/homebrew/bin # https://brew.sh/
 fish_add_path /opt/homebrew/sbin
 fish_add_path /opt/homebrew/opt/sqlite/bin
+fish_add_path /opt/homebrew/opt/openjdk/bi
 fish_add_path $GOPATH/bin
 fish_add_path $PNPM_HOME
 fish_add_path $HOME/.local/bin
