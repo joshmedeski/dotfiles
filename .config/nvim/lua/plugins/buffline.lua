230d2939ca10abbafa5d8f118e61c956c12332bb
Josh Medeski <josh.medeski@gmail.com>
2023-03-02 13:32:32 -0600

    feat[nvim]: simplify bufferline

---
+++ .config/nvim/lua/plugins/buffline.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      offsets = { text_align = "left" },
+      separator_style = { "", "" },
+      indicator = { style = "none" },
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      show_tab_indicators = false,
+      always_show_bufferline = true,
+    },
+  },
+}
