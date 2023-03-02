9b5fa8c9f2d5f2c61837000ca9f962e07f90ad31
Josh Medeski <josh.medeski@gmail.com>
2023-03-02 11:21:41 -0600

    fix: previewer

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-bat --color always "$@"
+#!/usr/bin/env bash
+
+case "$1" in
+*) bat --force-colorization --paging=never --style=changes,numbers \
+	--terminal-width $(($2 - 3)) "$1" && false ;;
+esac
