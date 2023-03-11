0aee6441691d0216a6e48bf2317a7577e75bc1de
Josh Medeski <josh.medeski@gmail.com>
2023-03-11 11:54:03 -0600

    feat[nvim]: switch to lsp lines

---
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -0,0 +1,4 @@
+return {
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  opts = {},
+}
---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    diagnostics = {
+      virtual_text = false,
+    },
+  },
+}
