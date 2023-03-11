a1115a75002769af755fe32daaf14b7341fc9623
Josh Medeski <josh.medeski@gmail.com>
2023-03-11 12:25:04 -0600

    feat: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-
+#
 # ███████╗██╗███████╗██╗  ██╗
 # ██╔════╝██║██╔════╝██║  ██║
 # █████╗  ██║███████╗███████║
@@ -12,31 +12,44 @@
 starship init fish | source # https://starship.rs/
 zoxide init fish | source # 'ajeetdsouza/zoxide'
 
-set -gx fish_greeting # disable fish greeting
 set -gx CPPFLAGS -I/opt/homebrew/opt/sqlite/include
 set -gx LDFLAGS -L/opt/homebrew/opt/sqlite/lib
 set -gx PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
 set -gx PNPM_HOME /Users/joshmedeski/Library/pnpm # https://pnpm.io/
+set -gx RIPGREP_CONFIG_PATH "$HOME/.config/rg/ripgreprc"
+
 set -Ux BAT_THEME Nord # 'sharkdp/bat' cat clone 
 set -Ux EDITOR nvim # 'neovim/neovim' text editor 
-set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
-
-set -Ux FZF_DEFAULT_OPTS "--reverse --no-info --pointer='' --marker='' \
---ansi --color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
-
-set -Ux FZF_TMUX_OPTS "-p --reverse --no-info --pointer='' --marker='' \
---ansi --color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
-
-set -Ux FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
-
+set -Ux fish_greeting # disable fish greeting
+set -U FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 set -Ux GOPATH (go env GOPATH) # https://golang.google.cn/
 set -Ux KIT_EDITOR /opt/homebrew/bin/nvim # https://www.scriptkit.com/
 set -Ux LANG en_US.UTF-8
 set -Ux LC_ALL en_US.UTF-8
 set -Ux NODE_PATH "~/.nvm/versions/node/v16.19.0/bin/node" # 'nvm-sh/nvm'
 set -Ux PAGER ~/.local/bin/nvimpager # 'lucc/nvimpager'
-set -Ux RIPGREP_CONFIG_PATH "$HOME/.config/rg/ripgreprc"
 set -Ux VISUAL nvim
+
+set -Ux FZF_DEFAULT_OPTS "--reverse \
+--border rounded \
+--no-info \
+--pointer='' \
+--marker=' ' \
+--ansi \
+--color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
+
+set -Ux FZF_TMUX_OPTS "-p \
+--reverse \
+--border rounded \
+--no-info \
+--pointer='' \
+--marker=' ' \
+--ansi \
+--color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
+
+set -Ux FZF_CTRL_R_OPTS "--border-label=' history ' \
+--header='ctrl-d: delete' \
+--prompt='  '"
 
 # ordered by priority - bottom up
 fish_add_path /opt/homebrew/bin # https://brew.sh/
