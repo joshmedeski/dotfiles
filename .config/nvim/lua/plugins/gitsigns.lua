b01470bf6c35237afcc2eee43925db6e8e593e5d
Josh Medeski <josh.medeski@gmail.com>
2023-03-11 11:53:51 -0600

    fix[nvim]: fix rexet buffer (duplicate) and reindent

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -18,22 +18,22 @@ return {
         vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
       end
 
-        -- stylua: ignore start
-        map("n", "]g", gs.next_hunk, "Next Hunk")
-        map("n", "[g", gs.prev_hunk, "Prev Hunk")
-        map({ "n", "v" }, "<leader>gg", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>gx", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-        map("n", "<leader>gG", gs.stage_buffer, "Stage Buffer")
-        map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
-        map("n", "<leader>gx", gs.reset_buffer, "Reset Buffer")
-        map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
-        map("n", "<leader>gd", gs.diffthis, "Diff This")
-        map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      map("n", "]g", gs.next_hunk, "Next Hunk")
+      map("n", "[g", gs.prev_hunk, "Prev Hunk")
+      map({ "n", "v" }, "<leader>gg", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>gx", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map("n", "<leader>gG", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>gX", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+      map("n", "<leader>gb", function()
+        gs.blame_line({ full = true })
+      end, "Blame Line")
+      map("n", "<leader>gd", gs.diffthis, "Diff This")
+      map("n", "<leader>gD", function()
+        gs.diffthis("~")
+      end, "Diff This ~")
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
     end,
   },
-  keys = function()
-    return {}
-  end,
 }
