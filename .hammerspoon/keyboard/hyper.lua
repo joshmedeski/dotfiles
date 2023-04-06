4c4e8639e8dbf6eba1810bed1604d8c4bfc7ef48
Josh Medeski <josh.medeski@gmail.com>
2023-04-06 10:38:45 -0500

    feat: add n/p for hyper

--- .hammerspoon/keyboard/hyper.lua
+++ .hammerspoon/keyboard/hyper.lua
@@ -1,3 +1,4 @@
+-- cSpell:words eventtap
 local function hyper(key, keyEvent)
 	hs.hotkey.bind({ "alt", "shift", "cmd", "ctrl" }, key, function()
 		hs.eventtap.event.newKeyEvent(keyEvent, true):post()
@@ -8,3 +9,5 @@ hyper("h", "left")
 hyper("j", "down")
 hyper("k", "up")
 hyper("l", "right")
+hyper("n", "down")
+hyper("p", "up")
