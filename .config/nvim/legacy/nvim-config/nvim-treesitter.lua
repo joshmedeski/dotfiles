781172897f3aa8bdd3f554c21590cfee752fafc8
Josh Medeski <josh.medeski@gmail.com>
2023-04-06 12:44:06 -0500

    feat: update treesitter with markdown support

--- .config/nvim/legacy/nvim-config/nvim-treesitter.lua
+++ .config/nvim/legacy/nvim-config/nvim-treesitter.lua
@@ -1,6 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all"
-  ensure_installed = { "typescript", "lua" },
+  ensure_installed = { "typescript", "lua", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -22,6 +22,6 @@ require'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = { "markdown" },
   },
-}
+})
