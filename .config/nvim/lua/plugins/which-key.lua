2661a3d553a1060baad3f3e772d448f56d18b00f
Josh Medeski <josh.medeski@gmail.com>
2023-04-23 22:35:25 -0500

    feat: show z in which key (for folding)

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -13,7 +13,7 @@ return {
         text_objects = false, -- help for text objects triggered after entering an operator
         windows = false, -- default bindings on <c-w>
         nav = false, -- misc bindings to work with windows
-        z = false, -- bindings for folds, spelling and others prefixed with z
+        z = true, -- bindings for folds, spelling and others prefixed with z
         g = false, -- bindings for prefixed with g
       },
     },
