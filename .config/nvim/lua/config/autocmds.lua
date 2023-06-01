ab1400430474e1b86770bc6e7cc9c5d1b832e5eb
Josh Medeski <josh.medeski@gmail.com>
2023-06-01 10:36:54 -0500

    feat: add nospell to markdown files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -32,7 +32,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
   pattern = { "*.mdx", "*.md" },
   callback = function()
-    vim.cmd([[set filetype=markdown wrap linebreak nolist]])
+    vim.cmd([[set filetype=markdown wrap linebreak nolist nospell]])
   end,
 })
 
