cf01b13f02a144159afdf4221b05e533db2f5744
Josh Medeski <josh.medeski@gmail.com>
2023-06-21 22:39:53 -0500

    fix: treesitter context to properly work

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
+  event = { "BufReadPost", "BufNewFile" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = true,
+  opts = { enable = true },
   keys = {
     { "[c", "<cmd>lua require('treesitter-context').go_to_context()<cr>", desc = "Attach to the nearest test" },
   },
