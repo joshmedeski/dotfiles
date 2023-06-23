b4986f6427e86ca97f67bf60c9fe45a023851ed0
Josh Medeski <josh.medeski@gmail.com>
2023-06-23 17:18:57 -0500

    feat: update buffline settings

--- .config/nvim/lua/plugins/buffline.lua
+++ .config/nvim/lua/plugins/buffline.lua
@@ -2,13 +2,13 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
-      offsets = { text_align = "left" },
       separator_style = { "", "" },
+      offsets = { { text_align = "left", separator = false } },
       indicator = { style = "none" },
       show_buffer_close_icons = false,
       show_close_icon = false,
       show_tab_indicators = false,
-      always_show_bufferline = true,
+      always_show_bufferline = false,
     },
   },
 }
