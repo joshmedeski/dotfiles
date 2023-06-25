e93beeadb569d585ad9c1a8019739faf6fa53d66
Josh Medeski <josh.medeski@gmail.com>
2023-06-25 10:28:58 -0500

    chore: disable spelling

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,4 @@
+-- cSpell:disable
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
