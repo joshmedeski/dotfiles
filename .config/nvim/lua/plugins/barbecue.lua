db9365d0e95ed03887d0f26de4ee8dc536fb8861
Josh Medeski <josh.medeski@gmail.com>
2023-06-25 10:49:11 -0500

    feat: add barbecue

---
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,11 @@
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    theme = "catppuccin",
+  },
+}
