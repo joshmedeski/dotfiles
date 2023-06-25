a430ec62cbfa1f14160f92dbe309e5fd3b19820d
Josh Medeski <josh.medeski@gmail.com>
2023-06-25 10:49:20 -0500

    feat: style buffline to work with edgy

--- .config/nvim/lua/plugins/buffline.lua
+++ .config/nvim/lua/plugins/buffline.lua
@@ -1,14 +1,41 @@
 return {
   "akinsho/bufferline.nvim",
-  opts = {
-    options = {
-      separator_style = { "", "" },
-      offsets = { { text_align = "left", separator = false } },
-      indicator = { style = "none" },
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-      show_tab_indicators = false,
-      always_show_bufferline = false,
-    },
-  },
+  opts = function()
+    local Offset = require("bufferline.offset")
+    if not Offset.edgy then
+      local get = Offset.get
+      Offset.get = function()
+        if package.loaded.edgy then
+          local layout = require("edgy.config").layout
+          local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+          for _, pos in ipairs({ "left", "right" }) do
+            local sb = layout[pos]
+            if sb and #sb.wins > 0 then
+              local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+              ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+              ret[pos .. "_size"] = sb.bounds.width
+            end
+          end
+          ret.total_size = ret.left_size + ret.right_size
+          if ret.total_size > 0 then
+            return ret
+          end
+        end
+        return get()
+      end
+      Offset.edgy = true
+    end
+
+    return {
+      options = {
+        separator_style = { "", "" },
+        offsets = { { text_align = "left", separator = false } },
+        indicator = { style = "none" },
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        show_tab_indicators = false,
+        always_show_bufferline = false,
+      },
+    }
+  end,
 }
