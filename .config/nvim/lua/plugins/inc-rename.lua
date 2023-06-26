923a9dfe09ee0db7c52a3d2af0ac08389962a911
Josh Medeski <josh.medeski@gmail.com>
2023-06-26 08:36:22 -0500

    docs: add description to inc-rename mapping

--- .config/nvim/lua/plugins/inc-rename.lua
+++ .config/nvim/lua/plugins/inc-rename.lua
@@ -4,6 +4,6 @@ return {
     require("inc_rename").setup()
     vim.keymap.set("n", "<leader>rn", function()
       return ":IncRename " .. vim.fn.expand("<cword>")
-    end, { expr = true })
+    end, { expr = true, desc = "Incrementally rename current word" })
   end,
 }
---
+++ .config/nvim/lua/plugins/rayso.lua
@@ -0,0 +1,17 @@
+return {
+  "TobinPalmer/rayso.nvim",
+  cmd = "Rayso",
+  opts = {
+    base_url = "https://ray.so/", -- Default URL
+    open_cmd = "Arc", -- On MacOS, will open with open -a firefox.app. Other OS's are untested.
+    options = {
+      background = true, -- If the screenshot should have a background.
+      dark_mode = true, -- If the screenshot should be in dark mode.
+      logging_path = "", -- Path to create a log file in.
+      logging_file = "rayso", -- Name of log file, will be a markdown file, ex rayso.md.
+      logging_enabled = false, -- If you enable the logging file.
+      padding = 32, -- The default padding that the screenshot will have.
+      theme = "raindrop", -- Theme
+    },
+  },
+}
