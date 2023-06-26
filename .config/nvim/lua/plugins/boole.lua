c8acdc46677f4d31ccf9efc78ffda80da950846b
Josh Medeski <josh.medeski@gmail.com>
2023-06-26 10:20:31 -0500

    feat: add boole plugin

---
+++ .config/nvim/lua/plugins/boole.lua
@@ -0,0 +1,12 @@
+return {
+  "nat-418/boole.nvim",
+  opts = {
+    mappings = {
+      increment = "<C-a>",
+      decrement = "<C-x>",
+    },
+    additions = {
+      -- { "tic", "tac", "toe" },
+    },
+  },
+}
