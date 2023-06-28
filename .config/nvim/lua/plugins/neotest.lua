6983d99127668bd8f2b6231ef5dabda6a05a13da
Josh Medeski <josh.medeski@gmail.com>
2023-06-28 11:51:50 -0500

    refactor: insert neotest-vitest adapter
    Insert the adapter via a table insert to not overwrite the things
    LazyVim is already doing.
    
    Thanks to @elijahmanor for the tip:
    https://www.youtube.com/watch?v=7Nt8n3rjfDY

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,13 +1,10 @@
 return {
   "nvim-neotest/neotest",
-  dependencies = {
-    "marilari88/neotest-vitest",
-  },
-  config = function()
-    require("neotest").setup({ adapters = { require("neotest-vitest") } })
+  dependencies = { "marilari88/neotest-vitest" },
+  opts = function(_, opts)
+    table.insert(opts.adapters, require("neotest-vitest"))
   end,
   keys = {
-    -- { "<leader>t", "<cmd><cr>", desc = "" },
     { "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Attach to the nearest test" },
     { "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Toggle Test Summary" },
     { "<leader>to", "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = "Toggle Test Output Panel" },
