1d9ae85be4eb1eaee9b6637007ee51b7266266e1
Josh Medeski <josh.medeski@gmail.com>
2023-07-12 19:36:52 -0500

    chore: remove unused abbrevations
    I wasn't using these

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -4,20 +4,14 @@
 
 abbr ast "aw set -t (aw list | fzf-tmux -p --reverse --preview 'aw set -t {}')"
 
-abbr bc "brew cleanup"
-abbr bd "brew doctor"
 abbr bi "brew install"
 abbr bic "brew install --cask"
-abbr bif "brew info"
-abbr bifc "brew info --cask"
+abbr bin "brew info"
+abbr binc "brew info --cask"
 abbr bl "brew leaves"
 abbr blr "brew leaves --installed-on-request"
 abbr blp "brew leaves --installed-as-dependency"
-abbr bo "brew outdated"
-abbr bs "brew services"
-abbr bsr "brew services restart"
-abbr bu "brew update"
-abbr bug "brew upgrade"
+abbr bs "brew search"
 
 abbr c clear
 abbr cl clear
