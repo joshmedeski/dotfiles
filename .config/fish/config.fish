a879d6ce60f358f95975e5ba9814e51c28498ac2
Josh Medeski <josh.medeski@gmail.com>
2023-07-12 20:27:25 -0500

    chore: remove unused env variables in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,26 +14,18 @@ zoxide init fish | source # 'ajeetdsouza/zoxide'
 
 set -U fish_greeting # disable fish greeting
 set -U fish_key_bindings fish_vi_key_bindings
-set -U LANG en_US.UTF-8
-set -U LC_ALL en_US.UTF-8
+# set -U LANG en_US.UTF-8
+# set -U LC_ALL en_US.UTF-8
 
 set -Ux BAT_THEME Catppuccin-mocha # 'sharkdp/bat' cat clone
-set -Ux CPPFLAGS -I/opt/homebrew/opt/sqlite/include
 set -Ux EDITOR nvim # 'neovim/neovim' text editor
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
-set -Ux GOPATH (go env GOPATH) # https://golang.google.cn/
-set -Ux KIT_EDITOR /opt/homebrew/bin/nvim # https://www.scriptkit.com/
-set -Ux LDFLAGS -L/opt/homebrew/opt/sqlite/lib
 set -Ux PAGER "~/.local/bin/nvimpager" # 'lucc/nvimpager'
-set -Ux PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
-set -Ux PNPM_HOME /Users/joshmedeski/Library/pnpm # https://pnpm.io/
-set -Ux RIPGREP_CONFIG_PATH "$HOME/.config/rg/ripgreprc"
 set -Ux VISUAL nvim
 
-# ordered by priority - bottom up
+# golang - https://golang.google.cn/
+set -Ux GOPATH (go env GOPATH)
 fish_add_path $GOPATH/bin
-fish_add_path $PNPM_HOME
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.config/tmux/plugins/tmux-nvr/bin
+
 fish_add_path $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin
 fish_add_path $HOME/.config/bin # my custom scripts
