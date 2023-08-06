b552376b667190494aa0994e9ebc5f89813fbe1a
Josh Medeski <josh.medeski@gmail.com>
2023-08-06 16:39:00 -0500

    chore: simplify auto commands

--- .config/nvim/legacy/lua/joshmedeski/autocmd.lua
+++ .config/nvim/legacy/lua/joshmedeski/autocmd.lua
@@ -1,30 +1,14 @@
--- cSpell:words autocmd linebreak nolist afile yabairc skhdrc
-
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "*tmux.conf" },
   command = "execute 'silent !tmux source <afile> --silent'",
 })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { ".yabairc" },
-  command = "!brew services restart yabai",
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { ".skhdrc" },
-  command = "!brew services restart skhd",
-})
-
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
+vim.api.nvim_create_autocmd({
   pattern = { "*.mdx", "*.md" },
   callback = function()
-    vim.cmd([[set filetype=markdown wrap linebreak nolist]])
+    vim.o.filetype = "markdown"
+    vim.o.wrap = true
+    vim.o.linebreak = true
+    vim.o.list = false
   end,
-})
-
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-  pattern = { "gitmux.conf" },
-  callback = function()
-    vim.cmd([[set filetype=sh]])
-  end,
-})
+}, { "BufNewFile", "BufFilePre", "BufRead" })
