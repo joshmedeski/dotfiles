c0c0d2a9d6a44ee5e98539c026fdd5441fc3ffef
Josh Medeski <josh.medeski@gmail.com>
2023-08-06 12:53:49 -0500

    fix[nvim]: diffview loading by adding cmds

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,6 +1,13 @@
 return {
   "sindrets/diffview.nvim",
   dependencies = "nvim-lua/plenary.nvim",
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewClose",
+    "DiffviewToggleFiles",
+    "DiffviewFocusFiles",
+    "DiffviewRefresh",
+  },
   keys = {
     { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "NeoTree" },
     { "<leader>dx", "<cmd>DiffviewClose<cr>", desc = "NeoTree" },
