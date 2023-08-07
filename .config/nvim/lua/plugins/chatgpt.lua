1f6b9b0cdc44f699f2488c14a6a70890ca689d69
Josh Medeski <josh.medeski@gmail.com>
2023-08-07 10:21:03 -0500

    feat: update chatGPT cmds and add key maps

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -5,15 +5,24 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  cmd = { "ChatGPT", "ChatGPTActAs", "ChatGPTEditWithInstructions", "ChatGPTRun" },
+  cmd = {
+    "ChatGPT",
+    "ChatGPTActAs",
+    "ChatGPTCompleteCode",
+    "ChatGPTEditWithInstructions",
+    "ChatGPTRun",
+  },
+
   config = function()
     require("chatgpt").setup({
       api_key_cmd = "op read op://Personal/ChatGPT/api_key --no-newline",
     })
   end,
+
   keys = function()
     local chatgpt = require("chatgpt")
     local wk = require("which-key")
+
     wk.register({
       a = {
         function()
@@ -25,5 +34,10 @@ return {
       prefix = "<leader>",
       mode = "v",
     })
+
+    return {
+      { "<leader>aa", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
+      { "<leader>ac", "<cmd>ChatGPTCompleteCode<cr>", desc = "ChatGPTCompleteCode" },
+    }
   end,
 }
