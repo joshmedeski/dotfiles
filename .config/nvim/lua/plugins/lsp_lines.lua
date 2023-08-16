5a3968fdd68ee9ae75bbba35cfb9e59925ef9f70
Josh Medeski <josh.medeski@gmail.com>
2023-08-16 16:58:40 -0500

    feat: add lsp lines toggle

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -1,4 +1,7 @@
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   opts = {},
+  keys = {
+    { "<leader>cL", "<cmd>require('lsp_lines').toggle<cr>", desc = "Toggle LSP Lines" },
+  },
 }
