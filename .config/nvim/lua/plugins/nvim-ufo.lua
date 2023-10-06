67b2fdc65be658ab5cda64a56c2031f1842fdd16
Josh Medeski <josh.medeski@gmail.com>
2023-10-06 11:19:30 -0500

    feat: enable nvim ufo

---
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,68 @@
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
+end
+
+return {
+  "kevinhwang91/nvim-ufo",
+  enabled = true,
+  dependencies = { "kevinhwang91/promise-async" },
+  event = "BufRead",
+  keys = {
+    {
+      "zR",
+      function()
+        require("ufo").openAllFolds()
+      end,
+    },
+    {
+      "zM",
+      function()
+        require("ufo").closeAllFolds()
+      end,
+    },
+    {
+      "K",
+      function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end,
+    },
+  },
+  config = function()
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.foldcolumn = "0"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 100
+    vim.o.foldenable = true
+    require("ufo").setup({
+      fold_virt_text_handler = handler,
+      close_fold_kinds = { "imports" },
+    })
+  end,
+}
