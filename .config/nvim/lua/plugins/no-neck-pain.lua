e21ab66ef24295ecbb35741c9ec318f6bf780563
Josh Medeski <josh.medeski@gmail.com>
2023-10-12 10:17:40 -0500

    chore: remove unused plugin

--- .config/nvim/lua/plugins/no-neck-pain.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "shortcuts/no-neck-pain.nvim",
-  cmd = { "NoNeckPain", "NoNeckPainResize", "NoNeckPainWidthUp", "NoNeckPainWidthDown" },
-  config = function()
-    require("no-neck-pain").setup({})
-  end,
-}
