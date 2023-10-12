216c61bb7c017fd15dd8524e8136b17e403a4697
Josh Medeski <josh.medeski@gmail.com>
2023-10-12 11:01:37 -0500

    fix: obsidian day folder

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -32,7 +32,7 @@ return {
     completion = { nvim_cmp = true },
 
     daily_notes = {
-      folder = "Calendar 🗓️/days",
+      folder = "Calendar 🗓️/Days 🌄",
       -- Optional, if you want to change the date format for the ID of daily notes.
       -- date_format = "%Y-%m-%d",
       -- Optional, if you want to change the date format of the default alias of daily notes.
