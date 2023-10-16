8b0120bf590a15fee2f91a4762873b925ef5859f
Josh Medeski <josh.medeski@gmail.com>
2023-10-16 10:54:46 -0500

    feat(lualine): use gitsigns for diff source

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -33,6 +33,16 @@ return {
       },
       colored = true,
       always_visible = false,
+      source = function()
+        local gitsigns = vim.b.gitsigns_status_dict
+        if gitsigns then
+          return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed,
+          }
+        end
+      end,
     }
 
     return {
