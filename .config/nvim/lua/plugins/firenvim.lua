b44221112a1d5b78122b1da1cbe0f98dad3b2f5e
Josh Medeski <josh.medeski@gmail.com>
2023-10-17 11:31:25 -0500

    fix: firevim

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -1,17 +1,17 @@
 return {
   "glacambre/firenvim",
-  cond = not not vim.g.started_by_firenvim,
+  -- cond = not vim.g.started_by_firenvim,
   build = function()
     require("lazy").load({ plugins = { "firenvim" }, wait = true })
     vim.fn["firenvim#install"](0)
   end,
-  config = function()
-    vim.g.firenvim_config = {
-      localSettings = {
-        [".*"] = {
-          takeover = "never",
-        },
-      },
-    }
-  end,
+  -- config = function()
+  --   vim.g.firenvim_config = {
+  --     localSettings = {
+  --       [".*"] = {
+  --         takeover = "never",
+  --       },
+  --     },
+  --   }
+  -- end,
 }
