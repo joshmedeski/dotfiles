cfe7b8b67d79c79b7aede9955c31324075913439
Josh Medeski <josh.medeski@gmail.com>
2023-10-29 17:43:51 -0500

    feat: add color overwrites to catppuchin

--- .config/nvim/lua/plugins/catppuchin.lua
+++ .config/nvim/lua/plugins/catppuchin.lua
@@ -1,51 +1,57 @@
+local icons = require("config.icons")
+
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   ---@class CatppuccinOptions
-  opts = {
-    flavour = "mocha",
-    transparent_background = true,
-    integrations = {
-      cmp = true,
-      fidget = true,
-      gitsigns = true,
-      harpoon = true,
-      lsp_trouble = true,
-      mason = true,
-      neotest = true,
-      noice = true,
-      notify = true,
-      octo = true,
-      telescope = {
-        enabled = true,
-      },
-      treesitter = true,
-      treesitter_context = false,
-      symbols_outline = true,
-      illuminate = true,
-      which_key = true,
-      barbecue = {
-        dim_dirname = true,
-        bold_basename = true,
-        dim_context = false,
-        alt_background = false,
-      },
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
+  opts = function()
+    local colors = require("config.colors")
+    return {
+      flavour = "mocha",
+      transparent_background = true,
+      color_overrides = { all = colors },
+      integrations = {
+        cmp = true,
+        fidget = true,
+        gitsigns = true,
+        harpoon = true,
+        lsp_trouble = true,
+        mason = true,
+        neotest = true,
+        noice = true,
+        notify = true,
+        octo = true,
+        telescope = {
+          enabled = true,
         },
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
+        treesitter = true,
+        treesitter_context = false,
+        symbols_outline = true,
+        illuminate = true,
+        which_key = true,
+        barbecue = {
+          dim_dirname = true,
+          bold_basename = true,
+          dim_context = false,
+          alt_background = false,
+        },
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
         },
       },
-    },
-  },
+    }
+  end,
 }
