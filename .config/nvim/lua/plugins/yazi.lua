d6e0ec5de22f40098eef7184f7af0aeb517d4165
Josh Medeski <josh.medeski@gmail.com>
2023-11-01 11:55:43 -0500

    feat: add yazi neovim plugin

---
+++ .config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,11 @@
+return {
+  "DreamMaoMao/yazi.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+
+  keys = {
+    { "<leader>fF", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+  },
+}
