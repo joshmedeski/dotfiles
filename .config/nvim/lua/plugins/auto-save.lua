d47817ae8aed8aad996b81be8fc7f76ff5c6a4d9
Josh Medeski <josh.medeski@gmail.com>
2023-11-12 17:25:16 -0600

    feat(nvim): adjust auto-save options

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,4 +1,34 @@
 return {
   "pocco81/auto-save.nvim",
-  opts = {},
+  opts = {
+    enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    execution_message = {
+      message = function() -- message to print on save
+        return ("AutoSaved at " .. vim.fn.strftime("%H:%M:%S"))
+      end,
+      dim = 0.18, -- dim the color of `message`
+      cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    },
+    trigger_events = { "InsertLeave", "FocusLost" }, -- vim events that trigger auto-save. See :h events function that determines whether to save the current buffer or not
+    -- return true: if buffer is ok to be saved
+    -- return false: if it's not ok to be saved
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+
+      if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+        return true -- met condition(s), can save
+      end
+      return false -- can't save
+    end,
+    write_all_buffers = false, -- write all buffers when the current one meets `condition`
+    debounce_delay = 3000, -- saves the file at most every `debounce_delay` milliseconds
+    callbacks = { -- functions to be executed at different intervals
+      enabling = nil, -- ran when enabling auto-save
+      disabling = nil, -- ran when disabling auto-save
+      before_asserting_save = nil, -- ran before checking `condition`
+      before_saving = nil, -- ran before doing the actual save
+      after_saving = nil, -- ran after doing the actual save
+    },
+  },
 }
