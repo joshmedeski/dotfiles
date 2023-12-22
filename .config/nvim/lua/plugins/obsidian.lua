658869869b090e0bb7ce721448bbe8766456474f
Josh Medeski <josh.medeski@gmail.com>
2023-12-22 09:29:49 -0600

    feat(obsidian): update nvim obsidian plugin config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -7,8 +7,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     -- "preservim/vim-markdown",
   },
-
-  lazy = true,
   event = {
     "BufReadPre " .. vim.fn.expand("~") .. "/repos/second-brain/**.md",
     "BufNewFile " .. vim.fn.expand("~") .. "/repos/second-brain/**.md",
@@ -39,18 +37,21 @@ return {
       -- alias_format = "%B %-d, %Y",
     },
 
+    disable_frontmatter = true,
+
+    -- TODO: configure to my liking
     -- Optional, alternatively you can customize the frontmatter data.
     note_frontmatter_func = function(note)
       -- This is equivalent to the default frontmatter function.
-      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-      end
-      return out
+      -- local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      -- -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- -- So here we just make sure those fields are kept in the frontmatter.
+      -- if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+      --   for k, v in pairs(note.metadata) do
+      --     out[k] = v
+      --   end
+      -- end
+      -- return out
     end,
 
     -- Optional, for templates (see below).
@@ -70,6 +71,40 @@ return {
 
     -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
     open_app_foreground = true,
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        -- Replace the above with this if you don't have a patched font:
+        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+        -- You can also add more custom ones...
+      },
+      external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = "ObsidianRefText" },
+      highlight_text = { hl_group = "ObsidianHighlightText" },
+      tags = { hl_group = "ObsidianTag" },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+        ObsidianTodo = { bold = true, fg = "#f78c6c" },
+        ObsidianDone = { bold = true, fg = "#89ddff" },
+        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+        ObsidianTilde = { bold = true, fg = "#ff5370" },
+        ObsidianRefText = { underline = true, fg = "#c792ea" },
+        ObsidianExtLinkIcon = { fg = "#c792ea" },
+        ObsidianTag = { italic = true, fg = "#89ddff" },
+        ObsidianHighlightText = { bg = "#75662e" },
+      },
+    },
   },
 
   -- mappings = {
