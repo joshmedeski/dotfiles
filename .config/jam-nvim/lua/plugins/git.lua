2385622ef6e3c3d37558981c9e7acc484e617758
Josh Medeski <josh.medeski@gmail.com>
2023-12-26 08:22:15 -0600

    feat(jam-nvim): add octo plugin

--- .config/jam-nvim/lua/plugins/git.lua
+++ .config/jam-nvim/lua/plugins/git.lua
@@ -79,5 +79,23 @@ return {
     -- `<leader>gy` (for normal and visual mode)
     dependencies = "nvim-lua/plenary.nvim",
     opts = {},
-  }
+  },
+
+  {
+    "pwntester/octo.nvim",
+  cmd = "Octo",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("octo").setup({ enable_builtin = true })
+    vim.treesitter.language.register("markdown", "octo")
+  end,
+  keys = {
+    { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
+  },
+}
+
 }
