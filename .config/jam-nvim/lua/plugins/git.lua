92cef2fc6099398885bc4960235d8527bf9d7e3a
Josh Medeski <josh.medeski@gmail.com>
2023-12-26 10:06:10 -0600

    chore(nvim): format

--- .config/jam-nvim/lua/plugins/git.lua
+++ .config/jam-nvim/lua/plugins/git.lua
@@ -83,19 +83,26 @@ return {
 
   {
     "pwntester/octo.nvim",
-  cmd = "Octo",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("octo").setup({ enable_builtin = true })
-    vim.treesitter.language.register("markdown", "octo")
-  end,
-  keys = {
-    { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
-  },
-}
-
+    cmd = "Octo",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({
+        enable_builtin = true,
+        mappings = {
+          review_diff = {
+            select_next_entry = { lhs = "<Tab>", desc = "move to previous changed file" },
+            select_prev_entry = { lhs = "<S-Tab>", desc = "move to next changed file" },
+          }
+        }
+      })
+      vim.treesitter.language.register("markdown", "octo")
+    end,
+    keys = {
+      { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
+    }
+  }
 }
