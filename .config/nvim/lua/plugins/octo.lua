bfc06dbcf8bf2ae5cc5f3d6c93e3a7bbc7081eb9
Josh Medeski <josh.medeski@gmail.com>
2023-12-26 10:05:10 -0600

    feat(nvim): map tab to cycle files in Octo review

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -7,7 +7,15 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("octo").setup({ enable_builtin = true })
+    require("octo").setup({
+      enable_builtin = true,
+      mappings = {
+        review_diff = {
+          select_next_entry = { lhs = "<Tab>", desc = "move to previous changed file" },
+          select_prev_entry = { lhs = "<S-Tab>", desc = "move to next changed file" },
+        }
+      }
+    })
     vim.treesitter.language.register("markdown", "octo")
   end,
   keys = {
