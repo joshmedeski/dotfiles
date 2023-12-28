332ee27f522f045a8e6e25700419d384b4d85d9d
Josh Medeski <josh.medeski@gmail.com>
2023-12-28 13:48:40 -0600

    chore: move glance to lspconfig depedency

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
+  depedencies = {
+    {
+      "dnlhc/glance.nvim",
+      cmd = "Glance",
+      ---@class GlanceOpts
+      opts = {
+        border = {
+          enable = true,
+          top_char = "―",
+          bottom_char = "―",
+        },
+      },
+    },
+  },
   ---@class PluginLspOpts
   opts = {
     diagnostics = { virtual_text = true },
