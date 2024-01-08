790db8c43e35196bc9a07f098399e28dbbc6b3d3
Josh Medeski <joshmedeski@Joshs-MacBook-Pro.local>
2024-01-08 10:10:22 -0600

    chore: update formatting

--- .config/nvim/lua/plugins/dark-notify.lua
+++ .config/nvim/lua/plugins/dark-notify.lua
@@ -2,11 +2,11 @@ return {
   "cormacrelf/dark-notify",
   init = function()
     require("dark_notify").run()
-    --   vim.api.nvim_create_autocmd("OptionSet", {
-    --     pattern = "background",
-    --     callback = function()
-    --       vim.cmd("Catppuccin " .. (vim.v.option_new == "light" and "latte" or "mocha"))
-    --     end,
-    --   })
+    vim.api.nvim_create_autocmd("OptionSet", {
+      pattern = "background",
+      callback = function()
+        vim.cmd("Catppuccin " .. (vim.v.option_new == "light" and "latte" or "mocha"))
+      end,
+    })
   end,
 }
