20998ab2655342e82083c502beb16a9c67b60614
Josh Medeski <josh.medeski@gmail.com>
2024-01-12 10:11:22 -0600

    feat(fish): configure zf

---
+++ .config/fish/conf.d/zf.fish
@@ -0,0 +1,10 @@
+#          _|
+# _  /  |
+#   /   __|
+# ___| _|
+# https://github.com/natecraddock/zf
+# a commandline fuzzy finder designed for filtering filepaths
+
+set -Ux FZ_PROMPT ' '
+set -Ux FZ_HIGHLIGHT green
+set -Ux FZ_VI_MODE true
