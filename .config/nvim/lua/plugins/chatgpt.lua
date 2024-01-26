9e10d83257a6669ff772dd199591dc7fa464262c
Josh Medeski <josh.medeski@gmail.com>
2024-01-26 10:32:19 -0600

    feat: update llm nvim plugins

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -1,44 +1,93 @@
 return {
   "jackMort/ChatGPT.nvim",
-  enabled = true,
+  enabled = false,
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "op read op://Personal/ChatGPT/api_key --no-newline",
+      openai_params = {
+        model = "gpt-3.5-turbo",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 1000,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+    })
+  end,
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  cmd = {
-    "ChatGPT",
-    "ChatGPTActAs",
-    "ChatGPTCompleteCode",
-    "ChatGPTEditWithInstructions",
-    "ChatGPTRun",
-  },
-
-  config = function()
-    require("chatgpt").setup({
-      api_key_cmd = "op read op://Personal/ChatGPT/api_key --no-newline",
-    })
-  end,
-
-  keys = function()
-    local chatgpt = require("chatgpt")
+  init = function()
     local wk = require("which-key")
-
     wk.register({
-      a = {
-        function()
-          chatgpt.edit_with_instructions()
-        end,
-        "Edit with instructions",
+      m = {
+        name = "+ChatGPT",
+        c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+        e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+        g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+        t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+        k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+        d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+        a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+        o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+        s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+        f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+        x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+        r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+        l = {
+          "<cmd>ChatGPTRun code_readability_analysis<CR>",
+          "Code Readability Analysis",
+          mode = { "n", "v" },
+        },
       },
-    }, {
-      prefix = "<leader>",
-      mode = "v",
-    })
-
-    return {
-      { "<leader>aa", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
-      { "<leader>ac", "<cmd>ChatGPTCompleteCode<cr>", desc = "ChatGPTCompleteCode" },
-    }
+    }, { prefix = "<leader>" })
   end,
 }
+
+-- return {
+--   "jackMort/ChatGPT.nvim",
+--   enabled = true,
+--   dependencies = {
+--     "MunifTanjim/nui.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "nvim-telescope/telescope.nvim",
+--   },
+--   cmd = {
+--     "ChatGPT",
+--     "ChatGPTActAs",
+--     "ChatGPTCompleteCode",
+--     "ChatGPTEditWithInstructions",
+--     "ChatGPTRun",
+--   },
+--
+--   config = function()
+--     require("chatgpt").setup({
+--       api_key_cmd = "op read op://Personal/ChatGPT/api_key --no-newline",
+--     })
+--   end,
+--
+--   keys = function()
+--     local chatgpt = require("chatgpt")
+--     local wk = require("which-key")
+--
+--     wk.register({
+--       a = {
+--         function()
+--           chatgpt.edit_with_instructions()
+--         end,
+--         "Edit with instructions",
+--       },
+--     }, {
+--       prefix = "<leader>",
+--       mode = "v",
+--     })
+--
+--     return {
+--       { "<leader>aa", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
+--       { "<leader>ac", "<cmd>ChatGPTCompleteCode<cr>", desc = "ChatGPTCompleteCode" },
+--     }
+--   end,
+-- }
--- .config/nvim/lua/plugins/gen.lua
+++ .config/nvim/lua/plugins/gen.lua
@@ -1,4 +1,11 @@
 return {
   "David-Kunz/gen.nvim",
   commands = { "Gen" },
+  opts = {
+    model = "mistral", -- The default model to use.
+    display_mode = "split", -- The display mode. Can be "float" or "split".
+    show_prompt = true, -- Shows the Prompt submitted to Ollama.
+    show_model = true, -- Displays which model you are using at the beginning of your chat session.
+    no_auto_close = false, -- Never closes the window automatically.
+  },
 }
