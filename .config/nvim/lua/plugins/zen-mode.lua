1b400886408d17d66bd30be39764d6e3fa552b91
Josh Medeski <josh.medeski@gmail.com>
2024-02-06 15:11:09 -0600

    feat: disable twilight in zen-mode

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -32,7 +32,7 @@ return {
         ruler = false, -- disables the ruler text in the cmd line area
         showcmd = false, -- disables the command in the last line of the screen
       },
-      twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+      twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
       gitsigns = { enabled = true }, -- disables git signs
       tmux = { enabled = true }, -- disables the tmux statusline
       -- this will change the font size on kitty when in zen mode
