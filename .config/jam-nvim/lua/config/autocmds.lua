7655c993e4746041f3d1f279392dc533a5247bf7
Josh Medeski <josh.medeski@gmail.com>
2024-03-04 20:37:39 -0600

    chore: remove autocmds I don't use anymore

--- .config/jam-nvim/lua/config/autocmds.lua
+++ .config/jam-nvim/lua/config/autocmds.lua
@@ -8,37 +8,12 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   command = "execute 'silent !source <afile> --silent'",
 })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { ".yabairc" },
-  command = "!yabai --restart-service",
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { ".skhdrc" },
-  command = "!brew services restart skhd",
-})
-
--- vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
---   pattern = { "bubu" },
---   callback = function()
---     vim.cmd([[set filetype=javascript]])
---   end,
--- })
-
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
   pattern = { "*.mdx", "*.md" },
   callback = function()
     vim.cmd([[set filetype=markdown wrap linebreak nolist nospell]])
   end,
 })
-
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-  pattern = { "*.conf" },
-  callback = function()
-    vim.cmd([[set filetype=sh]])
-  end,
-})
-
 
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
