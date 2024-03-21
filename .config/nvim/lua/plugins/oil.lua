d8c04197ed7af2af28baf8e534747a92d86152ae
Josh Medeski <josh.medeski@gmail.com>
2024-03-21 09:38:10 -0500

    feat: show hidden files in oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,9 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  opts = {
+    view_options = { show_hidden = true },
+  },
   init = function()
     vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
   end,
