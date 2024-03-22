3a90ea8963d8856621006a68dc5b24947eb5c576
Josh Medeski <josh.medeski@gmail.com>
2024-03-21 19:14:55 -0500

    feat: update gen

--- .config/nvim/lua/plugins/gen.lua
+++ .config/nvim/lua/plugins/gen.lua
@@ -1,11 +1,19 @@
 return {
   "David-Kunz/gen.nvim",
-  commands = { "Gen" },
-  opts = {
-    model = "mistral", -- The default model to use.
-    display_mode = "split", -- The display mode. Can be "float" or "split".
-    show_prompt = true, -- Shows the Prompt submitted to Ollama.
-    show_model = true, -- Displays which model you are using at the beginning of your chat session.
-    no_auto_close = false, -- Never closes the window automatically.
-  },
+  cmd = "Gen",
+  config = function()
+    local gen = require("gen")
+    gen.setup({
+      model = "codellama", -- The default model to use.
+      display_mode = "split", -- The display mode. Can be "float" or "split".
+      show_prompt = true, -- Shows the Prompt submitted to Ollama.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = true, -- Never closes the window automatically.
+    })
+    gen.prompts["Fix_Code"] = {
+      prompt = "Fix the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+      replace = true,
+      extract = "```$filetype\n(.-)```",
+    }
+  end,
 }
