09980dd23f7f255e3600b789457c34487502f1a2
Josh Medeski <josh.medeski@gmail.com>
2024-05-03 10:20:23 -0500

    disable filename for barbque

--- .config/nvim/lua/plugins/incline.lua
+++ .config/nvim/lua/plugins/incline.lua
@@ -53,16 +53,16 @@ return {
         },
       },
       render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-        local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "bold"
+        -- local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        -- local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+        -- local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "bold"
 
         local buffer = {
           { get_git_diff(props) },
           { get_diagnostic_label(props) },
-          { ft_icon, guifg = ft_color },
-          { " " },
-          { filename, gui = modified },
+          -- { ft_icon, guifg = ft_color },
+          -- { " " },
+          -- { filename, gui = modified },
         }
         return buffer
       end,
