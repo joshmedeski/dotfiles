9e921f8ad122d0b0654c0a66e6f62596e50bd585
Josh Medeski <josh.medeski@gmail.com>
2024-05-03 10:20:29 -0500

    feat: enable barbque

--- .config/nvim/lua/plugins/barbecue.lua
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -1,13 +1,19 @@
 return {
   "utilyre/barbecue.nvim",
-  name = "barbecue",
-  enabled = false,
   dependencies = {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons",
   },
+  name = "barbecue",
+  enabled = true,
+  event = "BufEnter",
   ---@type barbecue.Config
   opts = {
-    theme = "catppuccin",
+    -- TODO: trouble
+    theme = "catppuccin-frappe", -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+    show_navic = false,
+    symbols = {
+      separator = "/",
+    },
   },
 }
