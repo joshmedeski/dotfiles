e0eca5e81d7d0754aed498ae859ff4ad17b143c8
Josh Medeski <josh.medeski@gmail.com>
2024-07-11 15:42:20 -0500

    chore: add {} to setup

--- .config/nvim/lua/plugins/inc-rename.lua
+++ .config/nvim/lua/plugins/inc-rename.lua
@@ -2,7 +2,7 @@ return {
   "smjonas/inc-rename.nvim",
   cmd = "IncRename",
   config = function()
-    require("inc_rename").setup()
+    require("inc_rename").setup({})
     vim.keymap.set("n", "<leader>rn", function()
       return ":IncRename " .. vim.fn.expand("<cword>")
     end, { expr = true, desc = "Incrementally rename current word" })
