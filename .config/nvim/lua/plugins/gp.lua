06340c61f7aec2153d0307887268ea792cc22693
Josh Medeski <josh.medeski@gmail.com>
2024-07-15 16:27:41 -0500

    feat: add Claud 3 Haiku agent to gp

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -23,6 +23,10 @@ return {
         ollama = {
           endpoint = "http://localhost:11434/v1/chat/completions",
         },
+        anthropic = {
+          endpoint = "https://api.anthropic.com/v1/messages",
+          secret = os.getenv("ANTHROPIC_API_KEY"),
+        },
       },
       agents = {
         {
@@ -62,6 +66,22 @@ return {
           system_prompt = "You are an AI working as a code editor.\n\n"
             .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
             .. "START AND END YOUR ANSWER WITH:\n\n```",
+        },
+        {
+          name = "Claude3Haiku",
+          chat = true,
+          command = true,
+          provider = "anthropic",
+          model = { model = "claude-3-haiku-20240307" },
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
         },
       },
       hooks = {
