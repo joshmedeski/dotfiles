a420fb9638ebab26ebeeea12aba1505d443a7692
Josh Medeski <josh.medeski@gmail.com>
2024-08-13 00:04:35 -0500

    feat: switch arrow keys to alt shift hjkl

--- .hammerspoon/keyboard/arrows.lua
+++ .hammerspoon/keyboard/arrows.lua
@@ -1,10 +1,10 @@
-local function alt(bindKey, mapKey)
-	hs.hotkey.bind({ "alt" }, bindKey, function()
+local function altShift(bindKey, mapKey)
+	hs.hotkey.bind({ "alt", "shift" }, bindKey, function()
 		hs.eventtap.keyStroke({}, mapKey)
 	end)
 end
 
-alt("h", "left")
-alt("j", "down")
-alt("k", "up")
-alt("l", "right")
+altShift("h", "left")
+altShift("j", "down")
+altShift("k", "up")
+altShift("l", "right")
