79998cd0dd5e9042c1e504c6eb7e07563bab9acf
Josh Medeski <josh.medeski@gmail.com>
2024-10-30 22:12:16 -0500

    feat: update worktree nu scripts

--- .config/bin/nu-worktree-picker.sh
+++ .config/bin/nu-worktree-picker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd ~/c/nu
-# echo all dirs in ~/c/nu with new line separator
+cd ~/c/nu || exit
 WORKTREE=$(ls -d */ | tr -d '\n' | sed 's/\//\n/g' | gum filter --limit 1 --placeholder "Choose a Nutiliti worktree" --prompt="🏘️")
 echo "🏘️ $WORKTREE"
 
-cd ~/c/nu/$WORKTREE
-CHOICES=$(sesh list -z | grep "~/c/nu/$WORKTREE" | gum filter --placeholder "Choose sessions" --prompt="🏠")
+cd ~/c/nu/$WORKTREE || exit
+CHOICE=$(sesh list -z | grep "~/c/nu/$WORKTREE" | gum filter --limit 1 --placeholder "Choose sessions" --prompt="🏠")
 
-echo $CHOICES
+sesh connect "$CHOICE"
+tmux kill-session -t nutiliti\ worktrees
