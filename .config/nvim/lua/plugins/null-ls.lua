9fe3c718ea3744b17aab39eac11debf5452ad47e
Josh Medeski <josh.medeski@gmail.com>
2024-10-30 22:11:32 -0500

    feat: update markdownlint config path

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -4,6 +4,21 @@ return {
   dependencies = "neovim/nvim-lspconfig",
   config = function()
     local null_ls = require("null-ls")
+
+    local function get_markdownlint_config_path()
+      local cwd = vim.fn.getcwd()
+      local config_path = cwd .. "/.markdownlint.json"
+      local fallback_path = vim.fn.expand("~/.config/nvim/.markdownlint.json")
+
+      if vim.fn.filereadable(config_path) == 1 then
+        return config_path
+      elseif vim.fn.filereadable(fallback_path) == 1 then
+        return fallback_path
+      else
+        return nil
+      end
+    end
+
     null_ls.setup({
       debounce = 150,
       save_after_format = false,
@@ -15,7 +30,16 @@ return {
         null_ls.builtins.diagnostics.cspell,
         null_ls.builtins.diagnostics.fish,
         null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.diagnostics.markdownlint.with({
+          extra_args = function()
+            local config_path = get_markdownlint_config_path()
+            if config_path then
+              return { "--config", config_path }
+            else
+              return {}
+            end
+          end,
+        }),
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.fish_indent,
         null_ls.builtins.formatting.mdformat,
