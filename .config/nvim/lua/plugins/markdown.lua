2104041253a70fb15ca80574e78aacfba0127bf2
Josh Medeski <josh.medeski@gmail.com>
2024-11-09 16:49:15 -0600

    feat: update markdown styles

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,19 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
   opts = {
+    preset = "obsidian",
     file_types = { "markdown", "Avante" },
+    code = {
+      style = "language",
+      sign = false,
+      width = "full",
+      right_pad = 1,
+    },
+    heading = {
+      width = "block",
+      position = "overlay",
+    },
   },
   ft = { "markdown", "Avante" },
 }
