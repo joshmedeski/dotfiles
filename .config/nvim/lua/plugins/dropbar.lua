3aaea0a49130862ced0c41aa870fcb55e505f309
Josh Medeski <josh.medeski@gmail.com>
2025-01-13 11:12:26 -0600

    feat(nvim): add dropbar

---
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,28 @@
+return {
+  "Bekaboo/dropbar.nvim",
+  name = "dropbar",
+  event = "BufEnter",
+  opts = {
+    bar = {
+      sources = function()
+        local sources = require("dropbar.sources")
+        return { sources.path }
+      end,
+    },
+    icons = {
+      kinds = {
+        dir_icon = function()
+          return nil, nil
+        end,
+      },
+    },
+    sources = {
+      path = {
+        max_depth = 5,
+      },
+    },
+    symbol = {
+      on_click = function() end,
+    },
+  },
+}
