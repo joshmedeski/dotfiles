98c31d03eb240f99221e30c62e6b7bba87b97964
Josh Medeski <josh.medeski@gmail.com>
2025-01-16 22:25:59 -0600

    feat: add autocommands

--- .config/nvim-medeskivim/init.lua
+++ .config/nvim-medeskivim/init.lua
@@ -12,6 +12,66 @@ vim.cmd [[command! -nargs=0 GoToFile :lua Snacks.picker.files()]]
 vim.cmd [[command! -nargs=0 GoToSymbol :lua Snacks.picker.lsp_symbols()]]
 vim.cmd [[command! -nargs=0 Grep :lua Snacks.picker.grep()]]
 
+-- autocommands
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '*tmux.conf' },
+  command = "execute 'silent !tmux source <afile> --silent'",
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { 'yazi.toml' },
+  command = "execute 'silent !yazi --clear-cache'",
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { 'config.fish' },
+  command = "execute 'silent !source <afile> --silent'",
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { 'aerospace.toml' },
+  command = "execute 'silent !aerospace reload-config'",
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { 'sketchybarrc' },
+  command = '!brew services restart sketchybar',
+})
+
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufFilePre', 'BufRead' }, {
+  pattern = { '*.mdx', '*.md' },
+  callback = function()
+    vim.cmd [[set filetype=markdown wrap linebreak nolist nospell]]
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufRead' }, {
+  pattern = { '*.conf' },
+  callback = function()
+    vim.cmd [[set filetype=sh]]
+  end,
+})
+
+vim.api.nvim_create_augroup('HelpSplitRight', { clear = true })
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  group = 'HelpSplitRight',
+  pattern = '*',
+  callback = function()
+    if vim.bo.buftype == 'help' then
+      vim.cmd 'wincmd L'
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufRead' }, {
+  -- https://ghostty.org/docs/config/reference
+  pattern = { 'config' },
+  callback = function()
+    vim.cmd [[set filetype=toml]]
+    vim.cmd [[LspStop]]
+  end,
+})
+
 -- TODO: swap out gitsigns for mini.diff and mini.git
 
 -- Set <space> as the leader key
