16f213133f799ee7dab0a009b4c3931ebf93179b
Josh Medeski <josh.medeski@gmail.com>
2025-01-16 22:12:37 -0600

    feat: add more plugins and ⌘ bindings

--- .config/nvim-medeskivim/init.lua
+++ .config/nvim-medeskivim/init.lua
@@ -7,6 +7,13 @@
 ╚═╝     ╚═╝╚══════╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
 --]]
 
+vim.cmd [[command! -nargs=0 GoToCommand :lua Snacks.picker.command_history()]]
+vim.cmd [[command! -nargs=0 GoToFile :lua Snacks.picker.files()]]
+vim.cmd [[command! -nargs=0 GoToSymbol :lua Snacks.picker.lsp_symbols()]]
+vim.cmd [[command! -nargs=0 Grep :lua Snacks.picker.grep()]]
+
+-- TODO: swap out gitsigns for mini.diff and mini.git
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -106,6 +113,9 @@ vim.opt.splitkeep = 'screen'
 -- https://vimtricks.com/p/open-splits-more-naturally/
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+vim.o.cursorline = false
+vim.o.cursorlineopt = 'number'
 
 -- Make line numbers default
 vim.opt.number = false
@@ -272,11 +282,12 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = '' },
+        change = { text = '' },
+        delete = { text = '' },
+        topdelete = { text = '' },
+        changedelete = { text = '' },
+        untracked = { text = '' },
       },
     },
   },
@@ -435,27 +446,27 @@ require('lazy').setup({
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
@@ -793,9 +804,9 @@ require('lazy').setup({
           noice = true,
           notify = true,
           octo = true,
-          telescope = {
-            enabled = true,
-          },
+          -- telescope = {
+          --   enabled = true,
+          -- },
           treesitter = true,
           treesitter_context = false,
           symbols_outline = true,
--- .config/nvim-medeskivim/lua/custom/plugins/octo.lua
+++ .config/nvim-medeskivim/lua/custom/plugins/octo.lua
@@ -9,21 +9,20 @@ https://www.lazyvim.org/extras/util/octo
 LazyVim's Octo plugin for Git management with custom options and keybindings.
 --]]
 
--- TODO: replace with gh.nvim?
--- https://github.com/ldelossa/gh.nvim
 return {
-  "pwntester/octo.nvim",
+  'pwntester/octo.nvim',
   config = {
     mappings = {
       review_diff = {
         -- NOTE: make it easy to switch between files while reviewing diffs
-        select_next_entry = { lhs = "<Tab>", desc = "move to previous changed file" },
-        select_prev_entry = { lhs = "<S-Tab>", desc = "move to next changed file" },
+        select_next_entry = { lhs = '<Tab>', desc = 'move to previous changed file' },
+        select_prev_entry = { lhs = '<S-Tab>', desc = 'move to next changed file' },
       },
     },
   },
   keys = {
+    -- TODO: add Snacks support to Octo.nvim
     -- TODO: add this to the LazyVim extra (`<leader>go`?)
-    { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
+    { '<leader>o', '<cmd>Octo<cr>', desc = 'Octo' },
   },
 }
