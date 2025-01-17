f4b74a9931c7bfbd973b949c5f7cc079f23a91b1
Josh Medeski <josh.medeski@gmail.com>
2025-01-17 10:28:14 -0600

    fix: GoToFile to be smart picker

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,7 @@
 --]]
 
 vim.cmd [[command! -nargs=0 GoToCommand :lua Snacks.picker.command_history()]]
-vim.cmd [[command! -nargs=0 GoToFile :lua Snacks.picker.files()]]
+vim.cmd [[command! -nargs=0 GoToFile :lua Snacks.picker.smart()]]
 vim.cmd [[command! -nargs=0 GoToSymbol :lua Snacks.picker.lsp_symbols()]]
 vim.cmd [[command! -nargs=0 Grep :lua Snacks.picker.grep()]]
 
