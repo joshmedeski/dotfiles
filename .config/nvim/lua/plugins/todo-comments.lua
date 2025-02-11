5782967e32248214bff28d44f1d91f08918650bd
Josh Medeski <josh.medeski@gmail.com>
2025-02-11 14:14:51 -0600

    feat: add todo-comments config and keys

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,8 +1,32 @@
+--[[
+
+ ████████╗ ██████╗ ██████╗  ██████╗      ██████╗ ██████╗ ███╗   ███╗███╗   ███╗███████╗███╗   ██╗████████╗███████╗
+ ╚══██╔══╝██╔═══██╗██╔══██╗██╔═══██╗    ██╔════╝██╔═══██╗████╗ ████║████╗ ████║██╔════╝████╗  ██║╚══██╔══╝██╔════╝
+    ██║   ██║   ██║██║  ██║██║   ██║    ██║     ██║   ██║██╔████╔██║██╔████╔██║█████╗  ██╔██╗ ██║   ██║   ███████╗
+    ██║   ██║   ██║██║  ██║██║   ██║    ██║     ██║   ██║██║╚██╔╝██║██║╚██╔╝██║██╔══╝  ██║╚██╗██║   ██║   ╚════██║
+    ██║   ╚██████╔╝██████╔╝╚██████╔╝    ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║███████╗██║ ╚████║   ██║   ███████║
+    ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝      ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝
+
+    ✅ Highlight, list and search todo comments in your projects
+
+    Examples:
+
+     PERF: fully optimised
+     HACK: hmmm, this looks a bit funky
+     TODO: What else?
+     NOTE: adding a note
+     FIX: this needs fixing
+     WARNING: ???
+
+--]]
+
 return {
   'folke/todo-comments.nvim',
   event = { 'BufReadPost', 'BufNewFile' },
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = true },
+  opts = {
+    signs = false,
+  },
   keys = {
     {
       ']t',
@@ -17,6 +41,16 @@ return {
         require('todo-comments').jump_prev()
       end,
       desc = 'Previous todo comment',
+    },
+    {
+      '<leader>TA',
+      '<cmd>TodoTelescope<CR>',
+      desc = 'TodoTelescope',
+    },
+    {
+      '<leader>TT',
+      '<cmd>TodoTelescope keywords=TODO,FIX<CR>',
+      desc = 'TodoTelescope',
     },
   },
 }
