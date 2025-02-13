0cf0c1b93bb4bde967d0b7d0815cff666c9bb0ec
Josh Medeski <josh.medeski@gmail.com>
2025-02-13 16:57:22 -0600

    feat: update which key spec

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -45,13 +45,14 @@ return {
 
     -- Document existing key chains
     spec = {
-      { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-      { '<leader>d', group = '[D]ocument' },
-      { '<leader>r', group = '[R]ename' },
-      { '<leader>s', group = '[S]earch' },
-      { '<leader>w', group = '[W]orkspace' },
-      { '<leader>t', group = '[T]oggle' },
-      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      { '<leader>c', group = '[c]ode', mode = { 'n', 'x' } },
+      { '<leader>d', group = '[d]ocument' },
+      { '<leader>g', group = '[g]it', icon = { icon = '󰊢', color = 'green' } },
+      { '<leader>r', group = '[r]ename' },
+      { '<leader>s', group = '[s]earch' },
+      { '<leader>w', group = '[w]orkspace' },
+      { '<leader>t', group = '[t]oggle' },
+      { '<leader>h', group = '[h]unk (git)', mode = { 'n', 'v' } },
     },
   },
 }
