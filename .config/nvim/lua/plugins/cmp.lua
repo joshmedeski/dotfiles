c8265f7006b8b10610f2fd649773a75a7a9a2a14
Josh Medeski <josh.medeski@gmail.com>
2025-02-22 22:46:45 -0600

    feat(nvim): add lspkind and highlight colors to cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -6,6 +6,10 @@ return {
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
     {
+      'onsails/lspkind.nvim',
+      opts = {},
+    },
+    {
       'L3MON4D3/LuaSnip',
       build = (function()
         -- Build Step is needed for regex support in snippets.
@@ -35,12 +39,6 @@ return {
     --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-    {
-      'catgoose/nvim-colorizer.lua',
-      event = 'BufReadPre',
-      opts = { -- set to setup table
-      },
-    },
   },
   config = function()
     -- See `:help cmp`
@@ -118,6 +116,19 @@ return {
         { name = 'render-markdown' },
         { name = 'luasnip' },
         { name = 'path' },
+      },
+      formatting = {
+        format = function(entry, item)
+          local color_item = require('nvim-highlight-colors').format(entry, { kind = item.kind })
+          item = require('lspkind').cmp_format {
+            -- any lspkind format settings here
+          }(entry, item)
+          if color_item.abbr_hl_group then
+            item.kind_hl_group = color_item.abbr_hl_group
+            item.kind = color_item.abbr
+          end
+          return item
+        end,
       },
     }
   end,
