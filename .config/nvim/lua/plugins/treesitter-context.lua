c6c63e52d8fe04bf75ee09bef7d285857f92a96d
Josh Medeski <josh.medeski@gmail.com>
2025-04-20 20:20:36 -0500

    feat: enable treesitter context

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
   event = 'LazyFile',
+  enabled = false,
   opts = function()
     local tsc = require 'treesitter-context'
     Snacks.toggle({
