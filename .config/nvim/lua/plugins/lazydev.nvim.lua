97a58c917eba86056f6f47d2e60b2bf1bdc69406
Josh Medeski <josh.medeski@gmail.com>
2025-04-20 20:20:04 -0500

    feat: drop lazydev config

--- .config/nvim/lua/plugins/lazydev.nvim.lua
+++ .config/nvim/lua/plugins/lazydev.nvim.lua
@@ -14,14 +14,4 @@ return {
       },
     },
   },
-  { -- optional cmp completion source for require statements and module annotations
-    'hrsh7th/nvim-cmp',
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = 'lazydev',
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
-  },
 }
