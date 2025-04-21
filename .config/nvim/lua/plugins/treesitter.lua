aa8631eb94e8dbb700363ab47a1e982ce0aa0ac4
Josh Medeski <josh.medeski@gmail.com>
2025-04-20 20:22:25 -0500

    fix: treesitter context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,6 +12,25 @@ return {
         enable_autocmd = false,
       },
     },
+    {
+      'nvim-treesitter/nvim-treesitter-context',
+      event = 'BufReadPost',
+      opts = function()
+        local tsc = require 'treesitter-context'
+        Snacks.toggle({
+          name = 'Treesitter Context',
+          get = tsc.enabled,
+          set = function(state)
+            if state then
+              tsc.enable()
+            else
+              tsc.disable()
+            end
+          end,
+        }):map '<leader>ut'
+        return { mode = 'cursor', max_lines = 3 }
+      end,
+    },
   },
 
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
