1e2402fd2ff7a1abf60f5ebd2a84249f1cd5dc78
Josh Medeski <josh.medeski@gmail.com>
2025-04-25 10:01:50 -0500

    feat: remove unused snacks toggle

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,18 +16,6 @@ return {
       'nvim-treesitter/nvim-treesitter-context',
       event = 'BufReadPost',
       opts = function()
-        local tsc = require 'treesitter-context'
-        Snacks.toggle({
-          name = 'Treesitter Context',
-          get = tsc.enabled,
-          set = function(state)
-            if state then
-              tsc.enable()
-            else
-              tsc.disable()
-            end
-          end,
-        }):map '<leader>ut'
         return { mode = 'cursor', max_lines = 3 }
       end,
     },
