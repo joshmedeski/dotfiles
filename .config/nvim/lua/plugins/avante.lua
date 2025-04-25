1713673ae0b3f252f641b9f3a630b21e79117eb0
Josh Medeski <josh.medeski@gmail.com>
2025-04-25 10:02:17 -0500

    feat: update avante config

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -27,31 +27,7 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   enabled = true,
-  version = false, -- set this if you want to always pull the latest change
-
-  opts = {
-    provider = 'copilot',
-    cursor_applying_provider = nil,
-    behaviour = {
-      enable_cursor_planning_mode = true,
-    },
-    windows = {
-      postion = 'right',
-      width = 40,
-      sidebar_header = {
-        enabled = true,
-        align = 'center',
-        rounded = true,
-      },
-      input = {
-        prefix = ' ',
-        height = 12, -- Height of the input window in vertical layout
-      },
-    },
-    file_selector = {
-      provider = 'telescope',
-    },
-  },
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
 
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
@@ -60,6 +36,7 @@ return {
     'MunifTanjim/nui.nvim',
     'nvim-tree/nvim-web-devicons',
     'zbirenbaum/copilot.lua',
+    'ravitemer/mcphub.nvim',
     {
       -- support for image pasting
       'HakonHarnes/img-clip.nvim',
@@ -78,4 +55,77 @@ return {
       },
     },
   },
+
+  config = function()
+    require('avante').setup {
+      windows = {
+        postion = 'right',
+        width = 40,
+        sidebar_header = {
+          enabled = true,
+          align = 'left',
+          rounded = true,
+        },
+        input = {
+          prefix = ' ',
+          height = 12, -- Height of the input window in vertical layout
+        },
+      },
+
+      file_selector = {
+        provider = 'telescope',
+      },
+      selector = {
+        provider = 'telescope',
+      },
+
+      -- cursor_applying_provider = nil,
+      behaviour = {
+        enable_cursor_planning_mode = true,
+        enable_claude_text_editor_tool_mode = true,
+      },
+
+      provider = 'copilot',
+      copilot = {
+        model = 'claude-3.7-sonnet',
+      },
+      ollama = {
+        model = 'qwen2.5-coder:7b',
+      },
+
+      -- TODO: research what value this provides and setup correctly
+      -- rag_service = {
+      --   enabled = true, -- Enables the RAG service
+      --   host_mount = os.getenv 'HOME' .. '/github',
+      --   provider = 'ollama',
+      --   llm_model = 'deepseek-r1:8b',
+      --   endpoint = 'http://localhost:11434',
+      -- },
+
+      system_prompt = function()
+        local hub = require('mcphub').get_hub_instance()
+        return hub:get_active_servers_prompt()
+      end,
+
+      custom_tools = function()
+        return {
+          require('mcphub.extensions.avante').mcp_tool(),
+        }
+      end,
+
+      -- If you are using the builtin Neovim server, you might have to disable the following tools in your avante config to avoid any conflicts.
+      disabled_tools = {
+        'list_files',
+        'search_files',
+        'read_file',
+        'create_file',
+        'rename_file',
+        'delete_file',
+        'create_dir',
+        'rename_dir',
+        'delete_dir',
+        'bash',
+      },
+    }
+  end,
 }
