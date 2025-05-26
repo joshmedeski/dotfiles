a634c109baec2fc5731a44e645557ecf74dcf6af
Josh Medeski <joshmedeski@Joshs-MacBook-Pro.local>
2025-05-25 21:12:40 -0500

    🔧 Add Copilot cmd to copilot.vim

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -24,7 +24,8 @@ return {
   'github/copilot.vim',
   enabled = true,
   dependencies = { 'catppuccin/nvim' },
-  event = { "BufReadPost", "BufNewFile" },
+  cmd = { 'Copilot' },
+  event = { 'BufReadPost', 'BufNewFile' },
   init = function()
     vim.g.copilot_no_tab_map = false
     vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
