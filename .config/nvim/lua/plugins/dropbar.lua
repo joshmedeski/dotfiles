f021fc62697baac993393852e12276f476621def
Josh Medeski <josh.medeski@gmail.com>
2025-08-30 18:46:42 -0500

    Add lsp diagnostic counts to dropbar

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -1,20 +1,9 @@
--- TODO: find solution that can properly fetch per split
-local function get_gitsigns_stats()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed,
-    }
-  end
-end
-
 return {
   'Bekaboo/dropbar.nvim',
   enabled = true,
   event = 'BufEnter',
   name = 'dropbar',
+
   config = function()
     local bar = require 'dropbar.bar'
 
@@ -78,12 +67,75 @@ return {
     }
 
     ---@class dropbar_source_t
+    local lsp_diagnostics = {
+      get_symbols = function(buff, _, _)
+        local lspIcons = require('utils.icons').lsp
+
+        local errors = vim.diagnostic.get(buff, { severity = vim.diagnostic.severity.ERROR })
+        local warnings = vim.diagnostic.get(buff, { severity = vim.diagnostic.severity.WARN })
+        local infos = vim.diagnostic.get(buff, { severity = vim.diagnostic.severity.INFO })
+        local hints = vim.diagnostic.get(buff, { severity = vim.diagnostic.severity.HINT })
+
+        local stats = {}
+
+        if #errors > 0 then
+          table.insert(
+            stats,
+            bar.dropbar_symbol_t:new {
+              icon = lspIcons.error,
+              icon_hl = 'DiagnosticError',
+              name = tostring(#errors),
+              name_hl = 'DiagnosticError',
+            }
+          )
+        end
+
+        if #warnings > 0 then
+          table.insert(
+            stats,
+            bar.dropbar_symbol_t:new {
+              icon = lspIcons.error,
+              icon_hl = 'DiagnosticWarn',
+              name = tostring(#warnings),
+              name_hl = 'DiagnosticWarn',
+            }
+          )
+        end
+
+        if #infos > 0 then
+          table.insert(
+            stats,
+            bar.dropbar_symbol_t:new {
+              icon = lspIcons.info,
+              icon_hl = 'DiagnosticInfo',
+              name = tostring(#infos),
+              name_hl = 'DiagnosticInfo',
+            }
+          )
+        end
+
+        if #hints > 0 then
+          table.insert(
+            stats,
+            bar.dropbar_symbol_t:new {
+              icon = lspIcons.hint,
+              icon_hl = 'DiagnosticHint',
+              name = tostring(#hints),
+              name_hl = 'DiagnosticHint',
+            }
+          )
+        end
+
+        return stats
+      end,
+    }
+
+    ---@class dropbar_source_t
     require('dropbar').setup {
       bar = {
         sources = function()
           local sources = require 'dropbar.sources'
-
-          return { sources.path, gitsigns_stats }
+          return { sources.path, mini_diff_stats, lsp_diagnostics }
         end,
       },
     }
