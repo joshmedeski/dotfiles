5fdb2b9812fa543192816a800aefc3dfb83e56a5
Josh Medeski <josh.medeski@gmail.com>
2025-10-17 11:33:29 -0500

    Update sidekick config

--- .config/nvim/lua/plugins/sidekick.lua
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -1,65 +1,80 @@
 return {
   'folke/sidekick.nvim',
+  enabled = true,
+  event = 'VeryLazy',
+  cmds = { 'Sidekick' },
+  ---@class sidekick.Config
   opts = {
-    -- add any options here
     cli = {
       mux = {
-        backend = 'tmux',
         enabled = true,
+        create = 'window',
       },
     },
   },
   keys = {
-    -- {
-    --   '<tab>',
-    --   function()
-    --     -- if there is a next edit, jump to it, otherwise apply it if any
-    --     if not require('sidekick').nes_jump_or_apply() then
-    --       return '<Tab>' -- fallback to normal tab
-    --     end
-    --   end,
-    --   expr = true,
-    --   desc = 'Goto/Apply Next Edit Suggestion',
-    -- },
     {
       '<c-.>',
       function()
-        require('sidekick.cli').focus()
+        require('sidekick.cli').toggle()
       end,
-      mode = { 'n', 'x', 'i', 't' },
-      desc = 'Sidekick Switch Focus',
+      desc = 'Sidekick Toggle',
+      mode = { 'n', 't', 'i', 'x' },
     },
     {
       '<leader>aa',
       function()
-        require('sidekick.cli').toggle { focus = true }
+        require('sidekick.cli').toggle()
       end,
       desc = 'Sidekick Toggle CLI',
-      mode = { 'n', 'v' },
     },
+    {
+      '<leader>as',
+      function()
+        require('sidekick.cli').select()
+      end,
+      -- Or to select only installed tools:
+      -- require("sidekick.cli").select({ filter = { installed = true } })
+      desc = 'Select CLI',
+    },
+    {
+      '<leader>at',
+      function()
+        require('sidekick.cli').send { msg = '{this}' }
+      end,
+      mode = { 'x', 'n' },
+      desc = 'Send This',
+    },
+    {
+      '<leader>af',
+      function()
+        require('sidekick.cli').send { msg = '{file}' }
+      end,
+      desc = 'Send File',
+    },
+    {
+      '<leader>av',
+      function()
+        require('sidekick.cli').send { msg = '{selection}' }
+      end,
+      mode = { 'x' },
+      desc = 'Send Visual Selection',
+    },
+    {
+      '<leader>ap',
+      function()
+        require('sidekick.cli').prompt()
+      end,
+      mode = { 'n', 'x' },
+      desc = 'Sidekick Select Prompt',
+    },
+    -- Example of a keybinding to open Claude directly
     {
       '<leader>ac',
       function()
         require('sidekick.cli').toggle { name = 'claude', focus = true }
       end,
-      desc = 'Sidekick Claude Toggle',
-      mode = { 'n', 'v' },
-    },
-    {
-      '<leader>ag',
-      function()
-        require('sidekick.cli').toggle { name = 'grok', focus = true }
-      end,
-      desc = 'Sidekick Grok Toggle',
-      mode = { 'n', 'v' },
-    },
-    {
-      '<leader>ap',
-      function()
-        require('sidekick.cli').select_prompt()
-      end,
-      desc = 'Sidekick Ask Prompt',
-      mode = { 'n', 'v' },
+      desc = 'Sidekick Toggle Claude',
     },
   },
 }
