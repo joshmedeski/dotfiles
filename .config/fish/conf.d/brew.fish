0177c05d7fdc3790ec3a59a9d6e1049cdcd14ea4
Josh Medeski <josh.medeski@gmail.com>
2026-06-03 10:50:35 -0500

    Add tap trust require on homebrew

--- .config/fish/conf.d/brew.fish
+++ .config/fish/conf.d/brew.fish
@@ -1,6 +1,7 @@
 # Cached output of: /opt/homebrew/bin/brew shellenv
 # Regenerate with: /opt/homebrew/bin/brew shellenv > ~/.config/fish/conf.d/brew.fish
 set -gx HOMEBREW_PREFIX /opt/homebrew
+set -gx HOMEBREW_REQUIRE_TAP_TRUST 1
 set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
 set -gx HOMEBREW_REPOSITORY /opt/homebrew
 fish_add_path -gP /opt/homebrew/bin /opt/homebrew/sbin
